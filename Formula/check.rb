b3d01115dc2513753dd933e5c98c858d1b51bacd
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2024-10-07 13:07:29 +0200

    Update check.rb

--- Formula/check.rb
+++ Formula/check.rb
@@ -1,6 +1,6 @@
 class Check < Formula
   desc "Allows retrieving transaction information from the database."
-  homepage "Ajouter le lien du dossier github"
+  homepage "https://github.com/charleskoko/check-data-extractor"
   url "https://github.com/tonpseudo/homebrew-check/archive/v1.0.0.tar.gz"
   sha256 "le_checksum_du_tar_gz"
   version "1.0.0"
@@ -8,6 +8,8 @@ class Check < Formula
   depends_on "php"
 
   def install
-    bin.install "check.php" => "check"
-  end
+      bin.install "check.php" => "check"
+      # Install the src folder containing the necessary classes
+      (lib/"src").install Dir["src/*"]
+    end
 end
