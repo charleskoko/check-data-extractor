c89e68f43b20f255a335f9add0ac915556976708
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2024-10-07 13:21:20 +0200

    Update check.php

--- check.php
+++ check.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-echo "looking for this class" . __DIR__ . "/src/DatabaseConnection.php";
+echo "looking for this class" . __DIR__ . "/src.php";
 
 require_once __DIR__ . "/src/DatabaseConnection.php";
 require_once __DIR__ . "/src/TrackingLogs.php";
