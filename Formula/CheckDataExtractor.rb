28170f10d6e355dd0ad14154c6a8203aed891a2f
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2024-10-07 13:42:03 +0200

    Update CheckDataExtractor.rb

--- Formula/CheckDataExtractor.rb
+++ Formula/CheckDataExtractor.rb
@@ -1,7 +1,7 @@
-class Check < Formula
+class CheckDataExtractor < Formula
   desc "Allows retrieving transaction information from the database."
   homepage "https://github.com/charleskoko/check-data-extractor"
-  url "https://github.com/tonpseudo/homebrew-check/archive/v1.0.0.tar.gz"
+  url "https://github.com/charleskoko/check-data-extractor/archive/refs/tags/v1.2.0.tar.gz"
   sha256 "le_checksum_du_tar_gz"
   version "1.0.0"
 
